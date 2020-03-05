490584ec918abe02fa19e11a7e9623afe290b1d9
Joel <mang.hansson@gmail.com>
2020-03-05 16:18:12 +0100

    Create graph.c

---
+++ graph.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+struct Edge
+{
+    /* data */
+};
+
+
+struct node
+{
+    /* data */
+};
+
+
+struct graph
+{
+    /* data */
+};
+
+graph Empty(){
+
+}
+
+graph InsertNode(node v, graph g){
+
+}
+
+graph InsertEdge(edge e, graph g){
+
+}
+
+bool IsEmpty(graph g){
+
+}
+
+bool HasNoEdges(graph g){
+
+}
+
+node ChooseNode(graph g){
+
+}
+
+int Neighbours(node v, graph g){
+
+}
+
+void DeleteNode(node v, graph g){
+
+}
+
+void DeleteEdge(edge e, graph g){
+
+}
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
