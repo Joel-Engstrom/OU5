7ab31063b020683894ffcb5b2bbdadfefaac0fea
Joel <mang.hansson@gmail.com>
2020-03-09 15:44:25 +0100

    Revert "sg"
    This reverts commit 483ee01643b61f1d36ae36ba756efea19e7692d3.

--- src/is_connected.c
+++ src/is_connected.c
@@ -63,57 +63,6 @@ bool line_is_integer(const char *s)
     return isdigit(digit);
 }
 
-list *readCities(const char *map){
-    FILE *in;
-    char line[BUFSIZE];
-
-    //Try to open input file
-    in = fopen(map, "r");
-    if (in == NULL){
-        fprintf(stderr, "Failed to open %s for reading: %s\n", map, strerror(errno));
-        return -1;
-    }
-
-    list *l = list_empty(NULL);
-    int numCities = 0;
-    // Read a line at a time from the input file until EOF
-    while (fgets(line, BUFSIZE, in) != NULL) {
-        if (line_is_blank(line) || line_is_comment(line) || line_is_integer(line)) {
-            // Ignore blank lines and comment lines.
-            continue;
-        }
-        //Read first 3 letters
-        char *city = substring(line, 1, 3);
-
-        list_pos p = list_first(l);
-        bool duplicate = false;
-        if(list_is_empty(l)){
-            list_insert(l, city, list_end(l));
-            numCities++;
-            continue;
-        }
-        //Iterate through list to find duplicate
-        while (p != list_end(l)){
-            char *inspected_value = list_inspect(l, p);
-            if(!strcmp(city, inspected_value)){
-                duplicate = true;
-            }
-            p = list_next(l, p);
-        }
-        //Add city if no duplicate was found
-        if (!duplicate){
-            list_insert(l, city, list_end(l));
-            numCities++;
-        }
-    }
-    //Try to close input file
-    if (fclose(in)){
-        fprintf(stderr, "Failed to close %s: %s", map, strerror(errno));
-        return -1;
-    }
-    return l;
-}
-
 char *substring(const char *original, int start, int length)
 {
     char *city;
@@ -139,14 +88,52 @@ char *substring(const char *original, int start, int length)
 
 int main(int argc, const char **argv)
 {
+    FILE *in;
+    const char *map;
+    char line[BUFSIZE];
     //Verify number of parameters
     if (argc <= 1){
         fprintf(stderr, "Usage: ./isConnected airmap1.map\n");
         return -1;
     }
+    //Try to open input file
+    map = argv[1];
+    in = fopen(map, "r");
+    if (in == NULL){
+        fprintf(stderr, "Failed to open %s for reading: %s\n", map, strerror(errno));
+        return -1;
+    }
 
-    readCities(argv[1]);
+    list *l = list_empty(NULL);
+    int numCities = 0;
+    // Read a line at a time from the input file until EOF
+    while (fgets(line, BUFSIZE, in) != NULL) {
+        if (line_is_blank(line) || line_is_comment(line) || line_is_integer(line)) {
+            // Ignore blank lines and comment lines.
+            continue;
+        }
+        char *city = substring(line, 1, 3);
 
+        list_pos p = list_first(l);
+        bool duplicate = false;
+        if(list_is_empty(l)){
+            list_insert(l, city, list_end(l));
+            numCities++;
+            continue;
+        }
+        while (p != list_end(l)){
+            char *inspected_value = list_inspect(l, p);
+
+            if(!strcmp(city, inspected_value)){
+                duplicate = true;
+            }
+            p = list_next(l, p);
+        }
+        if (!duplicate){
+            list_insert(l, city, list_end(l));
+            numCities++;
+        }
+    }
     list_pos q = list_first(l);
     graph *g = graph_empty(numCities);
     while (q != list_end(l)){
